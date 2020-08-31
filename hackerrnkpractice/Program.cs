4e46a44d8b472b6a610ab746bcd9f01c3ba8bf7e
AadityaShivapurkar <aadishiva12413@gmail.com>
2020-08-31 06:56:09 +0530

    Update Program.cs

--- hackerrnkpractice/Program.cs
+++ hackerrnkpractice/Program.cs
@@ -619,3 +619,46 @@ class Solution
     }
 }*/
 
+//Day 14: Scope
+
+/*using System;
+using System.Linq;
+
+class Difference
+{
+    private int[] elements;
+    public int maximumDifference;
+
+    // Add your code here
+    public Difference(int[] elements)
+    {
+        this.elements = elements;
+    }
+
+    public void computeDifference()
+    {
+        Array.Sort(elements);
+        int size = elements.Count();
+        int min = elements[0];
+        int max = elements[size - 1];
+        maximumDifference = max - min;
+    }
+
+} // End of Difference Class
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        Convert.ToInt32(Console.ReadLine());
+
+        int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+        Difference d = new Difference(a);
+
+        d.computeDifference();
+
+        Console.Write(d.maximumDifference);
+    }
+}*/
+
